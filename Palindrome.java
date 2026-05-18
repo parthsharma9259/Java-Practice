720dface51b0f650c5dc8f3b1b4898f500e4d7c7
Parth Sharma <parthsharma4281@gmail.com>
2026-05-18 21:39:16 +0530

    Added palindrome java program

---
+++ Palindrome.java
@@ -0,0 +1,16 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        int num = 121, rev = 0, temp = num;
+
+        while(temp != 0){
+            int rem = temp % 10;
+            rev = rev * 10 + rem;
+            temp /= 10;
+        }
+
+        if(num == rev)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+}
