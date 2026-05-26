53f656abadb6c74c5b63579237aa84946974db2a
Parth Sharma <parthsharma4281@gmail.com>
2026-05-26 20:22:48 +0530

    Added Insertion Sort, Matrix Addition and Count Vowels Java programs

---
+++ MatrixAddition.java
@@ -0,0 +1,31 @@
+public class MatrixAddition {
+    public static void main(String[] args) {
+
+        int[][] a = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        int[][] b = {
+            {7, 8, 9},
+            {1, 2, 3}
+        };
+
+        int[][] sum = new int[2][3];
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                sum[i][j] = a[i][j] + b[i][j];
+            }
+        }
+
+        System.out.println("Sum of Matrix:");
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(sum[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
