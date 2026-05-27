6d54ccc838d6f6ea2ab92ee5946c246dafce0a4c
Parth Sharma <parthsharma4281@gmail.com>
2026-05-27 21:36:42 +0530

    Added 3 Java programs

---
+++ DuplicateElements.java
@@ -0,0 +1,17 @@
+public class DuplicateElements {
+    public static void main(String[] args) {
+
+        int arr[] = {1, 2, 3, 2, 4, 5, 1};
+
+        System.out.println("Duplicate Elements are:");
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] == arr[j]) {
+                    System.out.println(arr[i]);
+                }
+            }
+        }
+    }
+}
